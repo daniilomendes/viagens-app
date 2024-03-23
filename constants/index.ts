242a01cacca895dbd5ddfd09f9ab0cc14a83b01a
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-23 14:57:13 -0300

    feat: add constants

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Início" },
+  { href: "/", key: "how_hilink_work", label: "Como o Hilink funciona?" },
+  { href: "/", key: "serviços", label: "Serviços" },
+  { href: "/", key: "preços", label: "preços" },
+  { href: "/", key: "contact_us", label: "Fale Conosco" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Mapas reais podem ficar off-line",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Oferecemos uma solução para você poder usar nosso aplicativo na hora de escalar, sim mapas offline você pode usar a qualquer momento não há sinal no local",
+  },
+  {
+    title: "Definir um cronograma de aventura",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Agende uma aventura com os amigos. Nas férias, há muitas ofertas interessantes da Hilink. Assim, não há mais discussão",
+  },
+  {
+    title: "Tecnologia usando realidade aumentada",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "A tecnologia usa a realidade aumentada como um guia para sua trilha na floresta até o topo da montanha. Já suportado pela mais recente tecnologia sem ligação à Internet",
+  },
+  {
+    title: "Muitos novos locais todos os meses",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Muitos locais novos todos os meses, porque temos uma comunidade mundial de escaladores que partilham as suas melhores experiências com escalada",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Saiba mais",
+    links: [
+      "Sobre Hilink",
+      "Comunicados de imprensa",
+      "Ambiente",
+      "Empregos",
+      "Política de Privacidade",
+      "Contate-nos",
+    ],
+  },
+  {
+    title: "Nossa Comunidade",
+    links: ["Escalada", "Caminhada hilink", "Hilink do mês"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Fale conosco",
+  links: [
+    { label: "Número", value: "123-456-7890" },
+    { label: "Email", value: "danilo.mirandha@gmail.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Redes sociais",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
